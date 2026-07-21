4e309fcf7be9734d865258bc6b7536a8103f0ea7
suya mit <nay389956@gmail.com>
2026-07-21 19:53:36 +0900

    v5.33.18.92

--- main.cs
+++ main.cs
@@ -59,10 +59,10 @@ namespace TownOfHost
         public static ConfigEntry<string> ExplosionKeyInput { get; private set; }
 
         public const string PluginGuid = "com.satokazoku.TownOfHost-Pko";
-        public const string BepInExPluginVersion = "5.33.18.91";
-        public const string PluginVersion = "5.33.18.91";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
-        public const string PluginShowVersion = "5.33.18.91";
-        public const string ModVersion = ".18.91";//リリースver用バージョン変更dc9b79
+        public const string BepInExPluginVersion = "5.33.18.92";
+        public const string PluginVersion = "5.33.18.92";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
+        public const string PluginShowVersion = "5.33.18.92";
+        public const string ModVersion = ".18.92";//リリースver用バージョン変更dc9b79
 
         /// 配布するデバッグ版なのであればtrue。リリース時にはfalseにすること。
         public static bool DebugVersion = false;
