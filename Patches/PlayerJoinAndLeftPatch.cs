b23da2fea2eed2554dc72bd2ead1705c0bdfaa5b
kazoku sato <aoooni.nyanko828@gmail.com>
2026-08-09 18:44:40 +0900

    更新

--- Patches/PlayerJoinAndLeftPatch.cs
+++ Patches/PlayerJoinAndLeftPatch.cs
@@ -382,7 +382,8 @@ namespace TownOfHost
                 //var text4 = "\n\nコマンド一覧は「/cmd /h」と送信することで確認できます。";//"</size>\n<size=60%>\n☆参加型配信を行ったり、SNSで募集するのは?\n<size=50%>→<#352ac9>全然大丈夫です!!やっちゃってください!!</color>\n　<#fc8803>バニラAmongUsの公開ルーム</color>での<red>宣伝/勧誘/誘導</color>がダメなのです!!</size>";
                 var text5 = "";//"\n☆開発者から許可貰ってるって言ってる?　　\n<size=50%>→<#c9145a>個々で許可を出しておりません</color>!!大噓つきですよ!!</size>\n☆公開ルームに参加し、コード宣伝して「来てね～」って言うのは?\n<size=50%>→<#ff1919>勧誘/誘導</color>に当たるのでダメです。迷惑考えてくださいよ!!";
                 var text6 = "\n<size=60%><color=#7159A6>このMODはTOHKをフォークしたMODでTOHKとは別MODです。\n質問や不具合等が発生してもTOHKやTOHには報告しないでください。</color></size>";
-                Utils.SendMessage($"{text}{text2}{text5}{text6}", Sendto, $"<{Main.ModColor}>【This Room Use \"Town Of Host-Pko\"】</color>");
+                var text7 = "\n";
+                Utils.SendMessage($"{text}{text2}{text5}{text6}{text7}", Sendto, $"<{Main.ModColor}>【This Room Use \"Town Of Host-Pko\"】</color>");
             }
             else
             {
