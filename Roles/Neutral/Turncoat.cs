b0edd4178eab938d749bafb53239eb7b91f5e8df
Yoran_Furan <yon.yoran.f.2nd@gmail.com>
2026-08-18 17:49:32 +0900

    change : マドンナ/Jdの役職変化先からモノクラーを封印し、裏切り者を追加

--- Roles/Neutral/Turncoat.cs
+++ Roles/Neutral/Turncoat.cs
@@ -32,7 +32,7 @@ public sealed class Turncoat : RoleBase, IKiller
     )
     {
         cooldown = OptionCooldown.GetFloat();
-        duration = OptionCooldown.GetFloat();
+        duration = OptionDuration.GetFloat();
     }
     static OptionItem OptionCanTargetImpostor;
     static OptionItem OptionCanTargetNeutral;
@@ -78,6 +78,7 @@ public sealed class Turncoat : RoleBase, IKiller
             if (pc.PlayerId == Player.PlayerId) return false;
             if (pc.Is(CustomRoles.GM)) return false;
             if (pc.Is(CustomRoles.Turncoat)) return false;
+            if (pc.IsAlive() is false && GameStates.AfterIntro) return false;
 
             var role = pc.GetCustomRole().GetCustomRoleTypes();
 
