96dc25f265bd0e0a1e0e242aea6ca463798cb7f6
akebono-1382 <tiramisu334@outlook.com>
2026-08-18 11:22:05 +0900

    みつや専用処理削除

--- Roles/Neutral/JackalHadouHo.cs
+++ Roles/Neutral/JackalHadouHo.cs
@@ -174,12 +174,8 @@ public sealed class JackalHadouHo : RoleBase, ILNKiller, IUsePhantomButton, ISel
     public static bool GetTamaCanVentMove() => OptionTamaCanVentMove?.GetBool() ?? false;
     public static bool GetCanUseSabotageOption() => OptionCanSabotage?.GetBool() ?? false;
     public static bool GetTamaCountAsJackalKiller() => OptionTamaCountAsJackalKiller?.GetBool() ?? false;
-<<<<<<< Updated upstream
-=======
     public static bool GetTamaCountAsKillerOnlyWhenPromotionEnabled() => OptionTamaCountAsKillerOnlyWhenPromotionEnabled?.GetBool() ?? false;
     public static bool GetSidekickPromotion() => OptionSidekickPromotion?.GetBool() ?? false;
-
->>>>>>> Stashed changes
     public static void HideRoleOptions(CustomRoles role)
     {
         if (Options.CustomRoleSpawnChances != null && Options.CustomRoleSpawnChances.TryGetValue(role, out var sp))
