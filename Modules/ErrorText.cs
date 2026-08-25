9fb3991727b3a2a7dc386302e45bfe404b572852
akebono-1382 <tiramisu334@outlook.com>
2026-08-25 12:26:53 +0900

    オプションID重複エラーの優先度を3に戻す

--- Modules/ErrorText.cs
+++ Modules/ErrorText.cs
@@ -155,7 +155,7 @@ namespace TownOfHost
         // ==========
         // 001 Main
         Main_DictionaryError = 0010003, // 001-000-3 Main Dictionary Error
-        OptionIDDuplicate = 001_010_0, // 001-010-3 オプションIDが重複している(DEBUGビルド時のみ)
+        OptionIDDuplicate = 001_010_3, // 001-010-3 オプションIDが重複している(DEBUGビルド時のみ)
         OptionError = 001_020_1, // 001-020-1 オプションの読み込みに失敗した
         // 002 サポート関連
         UnsupportedVersion = 002_000_1,  // 002-000-1 AmongUsのバージョンが古い
