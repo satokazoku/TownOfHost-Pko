e0147d1d67596702595cfae1db4f21194feaacf2
kazoku sato <aoooni.nyanko828@gmail.com>
2026-08-28 17:10:23 +0900

    あけぼのさんのコミットに対応

--- main.cs
+++ main.cs
@@ -61,10 +61,10 @@ namespace TownOfHost
 
         public const string PluginGuid = "com.satokazoku.TownOfHost-Pko";
 
-        public const string BepInExPluginVersion = "5.34.21.96";
-        public const string PluginVersion = "5.34.21.96";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
-        public const string PluginShowVersion = "5.34.21.96";
-        public const string ModVersion = ".21.96";//リリースver用バージョン変更dc9b79
+        public const string BepInExPluginVersion = "5.34.21.97";
+        public const string PluginVersion = "5.34.21.97";//ほんとはx.y.z表記にしたかったけどx.y.z.km.ks表記だと警告だされる
+        public const string PluginShowVersion = "5.34.21.97";
+        public const string ModVersion = ".21.97";//リリースver用バージョン変更dc9b79
 
         /// 配布するデバッグ版なのであればtrue。リリース時にはfalseにすること。
         public static bool DebugVersion = false;
