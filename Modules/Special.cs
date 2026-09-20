250c933a54806b040e2b12d40b8da6c3120e2cfa
akebono-1382 <tiramisu334@outlook.com>
2026-09-20 11:49:41 +0900

    初期リリースの日付を変更

--- Modules/Special.cs
+++ Modules/Special.cs
@@ -17,7 +17,7 @@ static class Event
 
     public static bool IsChristmas = DateTime.Now.Month == 12 && DateTime.Now.Day is 24 or 25;
     public static bool White = DateTime.Now.Month == 3 && DateTime.Now.Day is 14;
-    public static bool IsInitialRelease = DateTime.Now.Month == 10 && DateTime.Now.Day is 31;
+    public static bool IsInitialRelease = DateTime.Now.Month == 4 && DateTime.Now.Day is 19;
     public static bool IsHalloween = (DateTime.Now.Month == 10 && DateTime.Now.Day is 31) || (DateTime.Now.Month == 11 && DateTime.Now.Day is 1 or 2 or 3 or 4 or 5 or 6 or 7);
     public static bool GoldenWeek = DateTime.Now.Month == 5 && DateTime.Now.Day is 3 or 4 or 5;
     public static bool April = DateTime.Now.Month == 4 && DateTime.Now.Day is 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8;
