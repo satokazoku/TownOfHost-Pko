23e23af3ca3008f18d16458bb178a3705d5cb551
akebono-1382 <tiramisu334@outlook.com>
2026-09-20 17:24:15 +0900

    EvilBlenderリストラ

--- Roles/Impostor/EvilBlender.cs
+++ Roles/Impostor/EvilBlender.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using System.Linq;
 using AmongUs.GameOptions;
 using HarmonyLib;
@@ -353,3 +353,4 @@ public sealed class EvilBlender : RoleBase, IImpostor, IUsePhantomButton
         achievements.Add(2, l1);
     }
 }
+*/
